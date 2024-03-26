c975bc07710f692724a05763d961b197e04b38f6
toddlf705 <toddlf705@gmail.com>
2024-03-26 16:25:23 -0400

    updated model fields

--- tick_it_project/tick_it/serializers.py
+++ tick_it_project/tick_it/serializers.py
@@ -4,7 +4,7 @@ from .models import Venue, Event
 class VenueSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Venue
-        fields = ('id', 'name', 'image', 'address', 'policies')
+        fields = ('id', 'name', 'image', 'address', 'policies', 'city', 'capacity', 'description', 'parking')
 
 class EventSerializer(serializers.HyperlinkedModelSerializer):
     venue = serializers.PrimaryKeyRelatedField(queryset=Venue.objects.all())
